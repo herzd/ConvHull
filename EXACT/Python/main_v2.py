10aaa90dacf80e58bd189775fa36bfb65e365e57
Daniel <daniel.herzig@outlook.at>
2021-07-09 17:25:06 +0200

    retangled main_v2.py to have up-to-date files

--- EXACT/Python/main_v2.py
+++ EXACT/Python/main_v2.py
@@ -1,17 +1,16 @@
 """
 Computes the convex hull for production envelopes of metabolic network. Solution is
 the list of hyperplanes and set of extreme points of the Convex hull. Inputs are:
-* fname: name of file without extension (must be the same for all files
-  - fname_r.txt: list of reaction names - order must follow that of S columns
-  - fname_S.txt: Stoichiometric matrix
-  - fname_d.txt : lb ub for each reaction
-* impt_reactions: list of indices for the dimensions onto which the CH should be computed
+  - REACTION_FILE : absolute path to file containing one reaction name string in each line, the names must be aligned with the columns of the Stoichiometric matrix.
+  - STOICHIOMETRIC_FILE : absolute path to file containing Stoichiometric matrix
+  - DOMAIN_FILE : absolute path to file containing lower and upper bounds for each reaction, one pair in a line
+  - INPUT_REACTIONS: list of indices for the dimensions onto which the CH should be computed
 """
 
 import chm_exact
 REACTION_FILE = "/home/daniel/ConvHull/DATA/toy/toy_reactions.txt"
 STOICHIOMETRIC_FILE = "/home/daniel/ConvHull/DATA/toy/toy_stoichs.txt"
 DOMAIN_FILE = "/home/daniel/ConvHull/DATA/toy/toy_domains.txt"
-REACTIONS = [0, 1]
+INPUT_REACTIONS = [0, 1]
 
-chm_exact.compute_CH(REACTION_FILE, STOICHIOMETRIC_FILE, DOMAIN_FILE, REACTIONS)
+chm_exact.compute_CH(REACTION_FILE, STOICHIOMETRIC_FILE, DOMAIN_FILE, INPUT_REACTIONS)
